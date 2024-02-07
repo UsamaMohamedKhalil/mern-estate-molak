1172357b6120b0c277e700f9e3bf115eee68a2cb
Usama Mohamed <usamamohamed1222@gmail.com>
2024-02-07 03:33:26 +0200

    Create Sign In API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User.model.js';
 import bcryptjs from 'bcryptjs';
-
+import  jwt  from 'jsonwebtoken';
+import { errorHandeler } from '../utils/error.js';
 export const signup = async (req , res , next) => {
    const {username , email , password} = req.body;
    const hashPassword = bcryptjs.hashSync(password,10);
@@ -13,3 +14,21 @@ export const signup = async (req , res , next) => {
    }
 
 };
+
+export const signin = async (req,res,next) => {
+   const {email,password} = req.body;
+   try{
+      const validUser = await User.findOne({email});
+      if(!validUser) return next(errorHandeler(404, 'User not found!'));
+      const validPassword = bcryptjs.compareSync(password , validUser.password);
+      if(!validPassword) return next(errorHandeler(401,'Wrong credentials'));
+      const token = jwt.sign({id : validUser._id} , process.env.JWT_SECRET);
+      const {password : pass , ...rest} = validUser._doc;
+      res
+      .cookie('access_token',token,{httpOnly : true})
+      .status(200)
+      .json(rest);
+   }catch(error){
+      next(error);
+   }
+}
