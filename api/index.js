e24971ef0aaf55b37c169a3324e4d12f28757565
Usama Mohamed <usamamohamed1222@gmail.com>
2024-02-12 00:25:23 +0200

    create user update API

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -11,7 +12,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
    console.log(err);
 });
 const app = express();
-
+app.use(cookieParser());
 app.use(express.json());
 app.listen(3000, ()=>{
    console.log("Server is running on port 3000!!");
