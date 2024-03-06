8df3c1a6d0193f436923c8e110ca9ac348d9a163
UsamaMohamedKhalil <usamamohamed1222@gmail.com>
2024-03-05 19:24:46 -0800

    Finish About Page and Home page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,9 @@
-import React from "react";
-
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Molak Estate</h1>
+      <p className='mb-4 text-slate-700'>Osama Mohamed Mahmoud presents a groundbreaking project aimed at revolutionizing real estate transactions. By leveraging the MERN stack, this web-based platform connects property owners directly, eliminating intermediaries and associated costs. With a focus on user-centric design and integrated AI capabilities, the platform offers personalized recommendations and enhances the property search experience. It prioritizes backend robustness and frontend accessibility while ensuring security measures and direct communication tools. This project seeks to democratize property transactions, making them more accessible, transparent, and cost-effective.</p>
+   
+    </div>
+  )
 }
